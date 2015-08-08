30ac7e9574d2d54b755a14db5741af94fec2b916
jason167 <jason5186@qq.com>
2015-08-08 11:05:29 +0800

    增加：自定义注解测试用例

---
+++ src/test/java/com/michael/annotation/PetServiceAnnotationTest.java
@@ -0,0 +1,55 @@
+package com.michael.annotation;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
+
+import com.michael.utils.PetAware;
+import com.michael.utils.PetMethod;
+
+public class PetServiceAnnotationTest  
+ {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	@Resource
+	private PetAware petServiceHolder;
+	
+	ApplicationContext context;
+	
+	@Before
+	public void init(){
+		try {
+			logger.info("init annotation test");
+			context = new ClassPathXmlApplicationContext("META-INF/application.xml");
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.error("", e);
+		}
+	}
+	
+	@Test
+	public void r(){
+		try {
+			
+			petServiceHolder = context.getBean("petServiceHolder", PetAware.class);
+			Assert.notNull(petServiceHolder, "ssdServiceHolder must not be null");
+			
+			String serviceType = "hello";
+			String version = "1.0.0";
+			PetMethod petMethod = petServiceHolder.getServiceMethod(serviceType, version);
+			logger.info("{}", petMethod.invoke("Michael", null));
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.error("", e);
+		}
+		
+	}
+
+}
