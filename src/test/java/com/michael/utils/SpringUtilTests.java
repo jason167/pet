fba81c864aea610ab2e9e960d1b7110ea5b1e3a4
jason167 <jason5186@qq.com>
2015-08-16 13:21:53 +0800

    增加：测试用例

--- src/test/java/com/michael/utils/SpringUtilTests.java
+++ src/test/java/com/michael/utils/SpringUtilTests.java
@@ -1,11 +1,28 @@
 package com.michael.utils;
 
+import javax.annotation.Resource;
+
+import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.michael.po.Cat;
 
 @TransactionConfiguration(defaultRollback = true)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-util.xml"}) 
 public class SpringUtilTests extends AbstractJUnit4SpringContextTests{
 
+	
+	@Resource
+	private Cat cat;
+	
+	@Test
+	public void testCatBean(){
+		if (cat == null) {
+			System.out.println("cat bean is null");
+		}else{
+			System.out.println("cat:" + cat);
+		}
+	}
 }
