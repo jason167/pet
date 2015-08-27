056d4917852773d6270908a0d93550e9766002ac
jason167 <jason5186@qq.com>
2015-08-27 11:15:22 +0800

    增加：BeanFactoryUtils类的应用

--- src/main/java/com/michael/utils/PetInitAware.java
+++ src/main/java/com/michael/utils/PetInitAware.java
@@ -1,8 +1,12 @@
 package com.michael.utils;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
@@ -12,6 +16,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerMapping;
 
 @Service("petInitAware")
 public class PetInitAware implements InitializingBean, ApplicationContextAware {
@@ -23,6 +28,22 @@ public class PetInitAware implements InitializingBean, ApplicationContextAware {
 			throws BeansException {
 		// TODO Auto-generated method stub
 		this.applicationContext = applicationContext;
+		listBean();
+	}
+	
+	void listBean(){
+		String[] beanNames = BeanFactoryUtils.beanNamesForTypeIncludingAncestors(applicationContext, Object.class);
+		for (String name : beanNames) {
+			logger.info("beanNames = name:{}", name);
+		}
+		
+		Map<String, Object> matchingBeans = BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, Object.class, true, false);
+		Iterator<String> iterator = matchingBeans.keySet().iterator();
+		
+		while(iterator.hasNext()){
+			String name = iterator.next();
+			logger.info("HandlerMapping = name:{}, bean:{}", name, matchingBeans.get(name));
+		}
 	}
 	
 	public void regService(Class<?> cls){
